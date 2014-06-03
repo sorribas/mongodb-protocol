60bf754e43207f676d04a3d6a1197c34d2a24d9d
Eduardo Sorribas <eduardo@sorribas.org>
2014-06-03 23:33:14 +0200

    Restructure test.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 var test = require('tape');
 var protocol = require('../index')
+var fs = require('fs');
+var net = require('net');
 
 test('should parse a reply from a mongodb server', function(t) {
   var p = protocol.parser();
 
-  var file = require('fs').readFileSync(__dirname + '/fixtures/command-ismaster', null);
-  var client = require('net').connect({port: 27017}, function() {
+  var file = fs.readFileSync(__dirname + '/fixtures/command-ismaster', null);
+  var client = net.connect({port: 27017}, function() {
     client.write(file);
     client.pipe(p);
   });
